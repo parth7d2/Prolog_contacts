849eae1c4262de67c26b16160a21e6fbfe261176
Parth <parth.comengineer@gmail.com>
2022-07-13 13:34:36 +0530

    CreateNewContactActivity, SplashActivity, App Icon, navigation bar color...

--- app/src/main/java/com/example/procontact/ui/contacts/CreateNewContactActivity.java
+++ app/src/main/java/com/example/procontact/ui/contacts/CreateNewContactActivity.java
@@ -16,7 +16,6 @@ import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
